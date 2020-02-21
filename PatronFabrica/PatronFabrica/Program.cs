cedc60500f61704dcdaff64a375f000bd42b9972
Jean Carlo Loza Carbajal <jeancarloloza@hotmail.com>
2020-02-21 16:03:13 -0600

    v1 sin patrones

--- PatronFabrica/PatronFabrica/Program.cs
+++ PatronFabrica/PatronFabrica/Program.cs
@@ -6,7 +6,39 @@ namespace PatronFabrica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string cConcepto = "";
+            string cImporte = "";
+            string cModalidad = "";
+            double dImporte = 0;
+            bool lAutorizado = false;
+
+            Console.WriteLine("Concepto del pedido");
+            cConcepto=Console.ReadLine();
+            Console.WriteLine("Importe del pedido");
+            cImporte = Console.ReadLine();
+            Console.WriteLine("Modalidad del pedido");
+            cModalidad = Console.ReadLine();
+
+            cModalidad = cModalidad.ToUpper();
+            dImporte = double.Parse(cImporte);
+
+            switch (cModalidad) {
+                case "EFECTIVO":
+                    cModalidad = "en efectivo";
+                    lAutorizado = dImporte <= 5000;
+                    break;
+                case "TARJETA":
+                    cModalidad = "con tarjeta";
+                    lAutorizado = dImporte > 5000;
+                    break;
+                default:
+                    throw new Exception("Concepto Erroneo");
+                    break;
+            }
+
+            Console.WriteLine(string.Format(
+                "Pedido {0} con importe de {1} {2} fue {3} ",
+                cConcepto,cImporte,cModalidad, lAutorizado ? "autorizado" : "rechazado"));
         }
     }
 }
